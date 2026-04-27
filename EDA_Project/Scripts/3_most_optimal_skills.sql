4c48887800731eda7cfa141a3d710103c205a676
Vugar-Nasirli <vuqar.nesirli2002@gmail.com>
2026-04-27 14:46:51 +0400

    Edit README.md

--- EDA_Project/Scripts/3_most_optimal_skills.sql
+++ EDA_Project/Scripts/3_most_optimal_skills.sql
@@ -5,10 +5,8 @@ AS
   SELECT
     s.skill_id,
     s.skills,
-    ROUND(MEDIAN(f.salary_year_avg), 0) AS median_year_salary,
-    ROUND(MEDIAN(f.salary_hour_avg), 0) AS median_hour_salary,
-    COUNT(f.salary_hour_avg) AS job_count_hour,
-    COUNT(f.salary_year_avg) AS job_count_year
+    ROUND(MEDIAN(f.salary_year_avg), 0) AS median_salary,
+    COUNT(f.job_id) AS job_count
   FROM
     job_postings_fact AS f
     LEFT JOIN skills_job_dim AS sj ON sj.job_id = f.job_id
@@ -24,48 +22,43 @@ AS
   SELECT
     skill_id,
     skills,
-    median_year_salary,
-    median_hour_salary,
-    job_count_hour,
-    job_count_year,
-    median_year_salary * job_count_year AS optimal_score_year,
-    median_hour_salary * job_count_hour AS optimal_score_hour
+    median_salary,
+    job_count,
+    ROUND(LN(median_salary * job_count / 10_000_000), 2) AS optimal_score
   FROM
     base_query
+  WHERE
+    skill_id IS NOT NULL
 )
 
 SELECT
   skill_id,
   skills,
-  median_year_salary,
-  job_count_year
-  --optimal_score_year,
-  --median_hour_salary,
-  --job_count_hour,
-  --optimal_score_hour
+  median_salary,
+  job_count,
+  optimal_score
 FROM
   optimal_scores
 ORDER BY
-  optimal_score_year DESC,
-  optimal_score_hour DESC
+  optimal_score DESC
 LIMIT 10;
 
 
 /*
-┌──────────┬───────────┬────────────────────┬────────────────┐
-│ skill_id │  skills   │ median_year_salary │ job_count_year │
-│  int32   │  varchar  │       double       │     int64      │
-├──────────┼───────────┼────────────────────┼────────────────┤
-│        1 │ python    │           127500.0 │          28943 │
-│        0 │ sql       │           120000.0 │          29027 │
-│       77 │ aws       │           136000.0 │          11308 │
-│        2 │ r         │           121700.0 │          11166 │
-│      183 │ tableau   │           112500.0 │          10830 │
-│       92 │ spark     │           140000.0 │           8649 │
-│       74 │ azure     │           130000.0 │           8625 │
-│      184 │ excel     │            90143.0 │           9858 │
-│       12 │ java      │           135000.0 │           6286 │
-│       73 │ snowflake │           137378.0 │           6049 │
-└──────────┴───────────┴────────────────────┴────────────────┘
-  10 rows                                          4 columns
+┌──────────┬──────────┬───────────────┬───────────┬───────────────┐
+│ skill_id │  skills  │ median_salary │ job_count │ optimal_score │
+│  int32   │ varchar  │    double     │   int64   │    double     │
+├──────────┼──────────┼───────────────┼───────────┼───────────────┤
+│        1 │ python   │      127500.0 │    759081 │          9.18 │
+│        0 │ sql      │      120000.0 │    758824 │          9.12 │
+│       77 │ aws      │      136000.0 │    302245 │          8.32 │
+│       74 │ azure    │      130000.0 │    280137 │           8.2 │
+│       92 │ spark    │      140000.0 │    222464 │          8.04 │
+│        2 │ r        │      121700.0 │    237602 │          7.97 │
+│      183 │ tableau  │      112500.0 │    241876 │          7.91 │
+│       12 │ java     │      135000.0 │    164723 │          7.71 │
+│      184 │ excel    │       90143.0 │    245645 │           7.7 │
+│      186 │ power bi │      102500.0 │    205785 │          7.65 │
+└──────────┴──────────┴───────────────┴───────────┴───────────────┘
+  10 rows                                               5 columns
 */
