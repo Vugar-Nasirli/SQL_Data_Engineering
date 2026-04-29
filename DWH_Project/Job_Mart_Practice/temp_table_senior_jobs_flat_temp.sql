099cc393b7af1c8de0085f7acb0535eecc3f1659
Vugar-Nasirli <vuqar.nesirli2002@gmail.com>
2026-04-30 01:16:59 +0400

    Delete Tracking Job_Mart_Practice

--- DWH_Project/Job_Mart_Practice/temp_table_senior_jobs_flat_temp.sql
+++
@@ -1,9 +0,0 @@
--- Creating and Loading Data into Temporary Table: senior_jobs_flat_temp
-CREATE OR REPLACE TEMPORARY TABLE senior_jobs_flat_temp
-AS
-SELECT
-    *
-FROM
-    main.priority_jobs_flat_view
-WHERE
-    job_title_short LIKE 'Senior%';
